4859f214380cc9a48271f3adca228bebfd72951f
Rapadapl <d.lysyenko@ya.ru>
2021-06-15 15:05:21 +1000

    Added new calculator parameter

--- src/main/java/Calculator/AbstractCalculator.java
+++ src/main/java/Calculator/AbstractCalculator.java
@@ -4,13 +4,25 @@ import java.util.List;
 
 public abstract class AbstractCalculator implements Operation {
    protected List<Integer> nums;
+    private String operationName;
 
     public AbstractCalculator(List<Integer> nums) {
         this.nums = nums;
     }
 
-
     public List<Integer> getNums() {
         return nums;
+    }
+
+    public void setNums(List<Integer> nums) {
+        this.nums = nums;
+    }
+
+    public String getOperationName() {
+        return operationName;
+    }
+
+    public void setOperationName(String operationName) {
+        this.operationName = operationName;
     }
 }
