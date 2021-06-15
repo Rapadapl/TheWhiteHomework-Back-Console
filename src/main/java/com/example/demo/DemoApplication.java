b41d10e28fe5b166dc31d1fd62feb9e438f26b0d
Rapadapl <d.lysyenko@ya.ru>
2021-06-15 15:05:49 +1000

    Setuping a context (for what?)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import com.example.demo.controller.ConsoleCalculatorController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
