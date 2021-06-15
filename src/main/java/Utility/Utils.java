1380bd66705bd21794edc79f970bdee1b1229edf
Rapadapl <d.lysyenko@ya.ru>
2021-06-15 14:02:49 +1000

    Some fixes

--- src/main/java/Utility/Utils.java
+++ src/main/java/Utility/Utils.java
@@ -1,12 +1,15 @@
 package Utility;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Utils {
 
     public static Boolean checkInput(String input) {
+        if (input != null)
         return !input.isEmpty() && input.matches("[0-9]+");
+        else return false;
     }
 
     public static List<Integer> strToIntList(String input) {
