fab46badc8e1dc3155527a1db8ededd8311de91b
Rapadapl <d.lysyenko@ya.ru>
2021-07-02 21:21:14 +1000

    Going to restful

--- src/main/java/com/example/demo/controller/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/controller/ApiExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.checker.CheckerException;
+import com.example.demo.controller.dto.ErrorDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class ApiExceptionHandler {
+    @ExceptionHandler(value = {CheckerException.class})
+    public ResponseEntity<Object> numbersBadRequest(RuntimeException exception) {
+        return new ResponseEntity<Object>(
+                new ErrorDto(exception.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/example/demo/controller/dto/ErrorDto.java
+++ src/main/java/com/example/demo/controller/dto/ErrorDto.java
@@ -1,4 +1,15 @@
 package com.example.demo.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorDto {
+
+    String message;
 }
