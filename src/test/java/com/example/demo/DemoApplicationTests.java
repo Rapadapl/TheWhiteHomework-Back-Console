69ac62f158de4b33c33908f4c845c08301dcc69e
Rapadapl <d.lysyenko@ya.ru>
2021-08-14 16:28:25 +1000

    added postgres integration

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import com.jupiter.tools.spring.test.postgres.annotation.meta.EnablePostgresIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnablePostgresIntegrationTest
 class DemoApplicationTests {
 
     @Test
